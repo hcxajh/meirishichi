404dc26c4b4d905a61055101205fbd5450125854
huacuo <zy110@2816>
2024-05-10 22:40:03 +0800

    增加了点击屏幕两侧可以随机翻页~

--- server.py
+++ server.py
@@ -16,14 +16,12 @@ def get_poems(page, page_size):
 
 
 random_page = random.randint(1, 10)
-
-
 def generate_html_files():
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('muban.htm')
     # 模拟的页面渲染过程
     for page in range(1, 11):
-        poems = get_poems(page, 1)  # 假设每页10首诗
+        poems = get_poems(page, 1)  # 假设每页1首诗
         rendered = template.render(page_number=page, poems=poems, total_pages=10, random_page=random_page)
         with open(f'{page}.html', 'w', encoding='utf-8') as file:
             file.write(rendered)
